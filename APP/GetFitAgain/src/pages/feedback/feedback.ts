139b0faf79b97af444f05e38936bca0165b33c92
Franz-Filip Schörghuber <franzfilip.schoerghuber@gmail.com>
2018-01-14 18:12:21 +0100

    Spielerei mit der APP
    Anbindung an die Datenbank und Anzeige in der APP

--- APP/GetFitAgain/src/pages/feedback/feedback.ts
+++ APP/GetFitAgain/src/pages/feedback/feedback.ts
@@ -10,39 +10,27 @@ import { FeedbackquestionsPage } from '../feedbackquestions/feedbackquestions';
 })
 export class FeedbackPage {
   //Variablen
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{name: string, note: string, icon: string}>;
   icons: string[];
-  selectedItem: any;
+  //selectedItem: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-
-
-    // Let's populate this page with some filler content for funzies
+    // Zufällig ausgewählt ob Objekt erledigt ist oder nicht
     this.icons = ['Erledigt', 'Unerledigt'];
 
     this.items = [];
     for (let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Feedback ' + i,
+        name: 'Feedback ' + i,
         note: 'This is item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
     //this.show();
     //var json_obj = JSON.parse(this.Getdata("http://api/person/1"));
-    var json_obj = this.Getdata("http://api/person/1");
+    //var json_obj = this.Getdata("http://api/person/1");
     //console.log("this is the name: "+json_obj.data.name);
     //console.log("DATA --> \n" + json_obj);
-  }
-
-  show() {
-    for (let i = 1; i < 11; i++) {
-      
-      this.items[i-1].title = "testfeedback";
-    }
   }
 
   Getdata(yourUrl){
@@ -52,11 +40,7 @@ export class FeedbackPage {
     return Httpreq.responseText;          
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    console.log("Jo da is wos gscheid fest druckt woadn");
-  }
-
+  //wenn ein item in der Liste ausgewählt wurde
   openFeedbackQuestions(){
     this.navCtrl.push(FeedbackquestionsPage);
     
